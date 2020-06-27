566db793f82335b790faf55515ca324d35144e1c
한은이 <haneuneee@gmail.com>
2020-06-27 19:29:52 +0900

    Add alias to base url - Add TypeScript config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import "./App.module.css";
+import React from "react";
+import "~/App.module.css";
 
 const App = () => {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import App from "~/App";
 
 if (module.hot) {
   module.hot.accept();
